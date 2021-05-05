1bf20749fb2a671af7e329ed8c2f7eb6b51def28
Sebastian <sebastian@squidex.io>
2021-05-05 08:47:18 +0200

    Added vultr

---
+++ vultr/install-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd /tmp
+
+echo "STEP 1: Install Docker"
+
+# Download script
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+# Execute script
+(. ./get-docker.sh)
+
+echo "STEP 2: Install Docker-Compose"
+
+# Install compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Apply executable permissions to the binary
+sudo chmod +x /usr/local/bin/docker-compose
+
